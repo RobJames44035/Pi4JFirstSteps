5ed14e2a21e1b15a28d10dbba05f7c5647e9cd1d
pi <robjames44035@gmail.com>
2024-01-14 09:53:39 -0500

    Add pi4j properties, pom.xml, Main.groovy, .gitignore files, and adjust resource files
    New properties for pi4j have been added, including the details for LED, Button, and Flash. A Maven pom.xml file was created, containing project information, dependencies, and build plugins. Implement a Main.groovy for Pi4J functionality. Also, an ignore file .gitignore was added to ignore .idea/ directory changes. The maven command instruction to create the project was moved to a new 'foo' file under 'src/main/resources' directory.

---
+++ src/main/groovy/com/rajames/Main.groovy
@@ -0,0 +1,87 @@
+package com.rajames
+
+
+import com.pi4j.Pi4J
+import com.pi4j.context.Context
+import com.pi4j.io.gpio.digital.DigitalInput
+import com.pi4j.io.gpio.digital.DigitalInputConfigBuilder
+import com.pi4j.io.gpio.digital.DigitalOutput
+import com.pi4j.io.gpio.digital.DigitalOutputConfigBuilder
+import com.pi4j.io.gpio.digital.DigitalState
+import com.pi4j.io.gpio.digital.PullResistance
+
+class Main {
+
+
+    private static int pressCount = 0
+
+    public static final String LED_PROVIDER = "pigpio-digital-output"
+    public static final String BUTTON_PROVIDER = "pigpio-digital-input"
+
+    static void main(String[] args) throws Exception {
+
+        Properties props = new Properties()
+        InputStream input = null
+
+        // Check if a command line argument exists (a file path for properties)
+        if (args.length != 0) {
+            try {
+                // Try to open the file
+                input = new FileInputStream(new File(args[0]))
+            } catch (FileNotFoundException ignored) {
+                println "Specified properties file not found."
+                System.exit(1)
+            }
+        } else {
+            // Default to bundled properties file in the jar
+            input = Thread.currentThread().getContextClassLoader().getResourceAsStream('pi4j.properties')
+        }
+
+        try {
+            input.withCloseable { InputStream stream ->
+                props.load(stream)
+            }
+        } catch (Exception ignored) {
+            println "Could not load properties."
+            System.exit(1)
+        }
+
+        Context pi4j = Pi4J.newAutoContext()
+
+        DigitalOutputConfigBuilder ledConfig = DigitalOutput.newConfigBuilder(pi4j)
+                .id(props.getProperty("led.id"))
+                .name(props.getProperty("led.name"))
+                .address(Integer.parseInt(props.getProperty("led.pin")))
+                .shutdown(DigitalState.LOW)
+                .initial(DigitalState.LOW)
+                .provider(LED_PROVIDER)
+        DigitalOutput led = pi4j.create(ledConfig)
+
+        DigitalInputConfigBuilder buttonConfig = DigitalInput.newConfigBuilder(pi4j)
+                .id(props.getProperty("button.id"))
+                .name(props.getProperty("button.name"))
+                .address(Integer.parseInt(props.getProperty("button.pin")))
+                .pull(PullResistance.PULL_DOWN)
+                .debounce(Long.parseLong(props.getProperty("button.debounce")))
+                .provider(BUTTON_PROVIDER)
+        DigitalInput button = pi4j.create(buttonConfig)
+        button.addListener(e -> {
+            if (e.state() == DigitalState.LOW) {
+                pressCount++
+            }
+        })
+
+        while (pressCount < Integer.parseInt(props.getProperty("flash.limit"))) {
+            //noinspection GrEqualsBetweenInconvertibleTypes
+            if (led == DigitalState.HIGH) {
+                led.low()
+            } else {
+                led.high()
+            }
+            Thread.sleep(500 / (pressCount + 1) as long)
+        }
+
+        // Shutdown Pi4J
+        pi4j.shutdown()
+    }
+}
